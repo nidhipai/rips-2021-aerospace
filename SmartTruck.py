626c3be5bbae905902172479bf78364f8df515cf
Salvador Balkus <salbalkus@gmail.com>
2021-06-23 17:12:42 -0700

    ND velocity simulation

--- SmartTruck.py
+++ SmartTruck.py
@@ -2,10 +2,12 @@ import numpy as np
 import DataGenerator
 
 
-class DumbTruck(DataGenerator.DataGenerator):
-    def __init__(self, xt0, ts, dt, ep, nu, k):
+class SmartTruck(DataGenerator.DataGenerator):
+    def __init__(self, xt0, ts, dt, ep, nu):
         super().__init__(xt0, ts, dt, ep, nu)
-        self.k = k
+        self.H = np.append(np.eye(self.n//2), np.zeros((self.n//2,self.n//2)), axis = 1)
+        self.A = np.append(np.append(np.eye(self.n//2), np.eye(self.n//2)*self.dt, axis=1), np.append(np.zeros((self.n//2,self.n//2)), np.eye(self.n//2), axis=1), axis=0)
+
 
     def process_step(self, xt_prev):
         """
@@ -13,7 +15,7 @@ class DumbTruck(DataGenerator.DataGenerator):
         :param xt_prev: Previous process state
         :return: State vector of next step in the process
         """
-        return xt_prev + self.dt*(np.matmul(np.array([[0, 1], [0, -self.k]]), xt_prev) + self.process_noise())
+        return np.matmul(self.A,xt_prev) + self.process_noise()
 
     def measure_step(self, xt):
         """
@@ -21,16 +23,16 @@ class DumbTruck(DataGenerator.DataGenerator):
         :param xt: Current state vector
         :return: State vector representing measure at the current process state
         """
-        return xt + self.measure_noise()
+        return np.matmul(self.H, xt) + self.measure_noise()
 
     def process_noise(self):
         """
         Generate process noise
         """
-        return np.array([[0], [np.random.normal(scale=np.sqrt(self.ep))]])
+        return np.append(np.zeros((self.n // 2, 1)), np.random.normal(scale=self.ep, size=(self.n // 2, 1)), axis=0)
 
     def measure_noise(self):
         """
         Generate measurement noise
         """
-        return np.array([[np.random.normal(scale=self.nu)], [np.random.normal(scale=np.sqrt(self.nu))]])
+        return np.random.normal(scale=self.nu, size=(self.n // 2, 1))
--- kalmanfilter2.py
+++ kalmanfilter2.py
@@ -9,7 +9,7 @@ import numpy.linalg as linalg
 
 
 class KalmanFilter:
-    def __init__(self, x_hat0, A, R, Q, H=None, B=0, u=0):
+    def __init__(self, x_hat0, A, Q, R, H=None, B=0, u=0):
         self.A = A  # state-transition matrix
         self.Q = Q  # process noise covariance
         self.R = R  # measurement noise covariance
@@ -28,7 +28,8 @@ class KalmanFilter:
             self.u = u
 
         # set a priori and a posteriori estimate error covariances to all ones (not all zeros)
-        self.P, self.P_minus = np.ones((self.n, self.n))
+        self.P = np.ones((self.n, self.n))
+        self.P_minus = np.ones((self.n, self.n))
         self.x_hat = x_hat0  # set a priori estimate to initial guess
         self.x_hat_minus = x_hat0  # set a posteriori estimate to initial guess
 
@@ -36,9 +37,9 @@ class KalmanFilter:
     def predict(self, measurement):
         self.x_hat_minus = np.dot(self.A,self.x_hat)
         self.P_minus = np.dot(np.dot(self.A,self.P),self.A.T) + self.Q
-        self.K = np.dot(self.P_minus,linalg.inv(self.P_minus + self.R))
-        self.x_hat = self.x_hat_minus + np.dot(self.K,(measurement - self.x_hat_minus))
-        self.P = np.dot((np.eye(self.n,self.n)- self.K),self.P_minus)
+        self.K = self.P_minus @ self.H.T @ linalg.inv(self.H @ self.P_minus @ self.H.T + self.R)
+        self.x_hat = self.x_hat_minus + np.dot(self.K,(measurement - np.matmul(self.H,self.x_hat_minus)))
+        self.P = (np.eye(self.n,self.n) - self.K @ self.H) @ self.P_minus
 
     #Return current a posteriori estimate
     def get_current_guess(self):
