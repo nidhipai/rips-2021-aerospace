cebac4590d1b6b9f292804bd1f6211b5a2639fa9
Nidhi Pai <nidhi.pai@gmail.com>
2021-06-29 16:12:30 -0700

    simulation update

--- simulation.py
+++ simulation.py
@@ -6,11 +6,11 @@ Simulation
 import numpy as np
 import matplotlib.pyplot as plt
 #plt.rcParams['text.usetex'] = True
+#import array_to_latex as a2l
 
-import random as random
-from palettable.colorbrewer.qualitative import Dark2_4
+import random
+#from palettable.colorbrewer.qualitative import Dark2_4
 from mpl_toolkits import mplot3d
-from SmartTruck import SmartTruck
 from kalmanfilter2 import KalmanFilter
 
 class Simulation:
@@ -70,7 +70,7 @@ class Simulation:
 		self.kFilter_model = self.kFilter(x0, f, jac, h, Q, R, H, u)
 		measures = []
 
-		for i in range(self.generator.ts):
+		for i in range(self.ts):
 			measure_t = self.measures[index][:, i]
 			measure_t.shape = (self.n//2, 1)
 			measures.append(measure_t)
@@ -80,7 +80,7 @@ class Simulation:
 		self.trajectories[len(self.trajectories.keys())] = output[:, 1:]  # delete the first column (initial data)
 
 	def plot(self, index=None, title="Object Position", x_label="x", y_label="y", z_label="z"):
-		color = Dark2_4.mpl_colors
+		#color = Dark2_4.mpl_colors
 		if index is None:
 			process = self.processes[len(self.processes.keys())-1]
 			measure = self.measures[len(self.measures.keys())-1]
@@ -95,24 +95,33 @@ class Simulation:
 		plt.rcParams.update({'font.size': 22})
 
 		if self.n//2 == 2:
-			title = "{}\n x0 = ({},{})\n Q={}, R={}\n seed={}".format(title, str(self.generator.xt0[0,0]), str(self.generator.xt0[1,0]), str(self.generator.Q), str(self.generator.R), self.seed_value)
-			plt.plot(process[0], process[1], lw=1.5, markersize = 15,color=color[2], marker=',')
-			plt.scatter(measure[0], measure[1], s = 50, lw=1.5,color=color[1], marker='+')
-			plt.plot(output[0], output[1], lw=0.4, markersize = 15, color=color[0], marker='.')
+			#title = "{}\n x0 = ({},{})\n Q={}, R={}\n seed={}".format(title, str(self.generator.xt0[0,0]), str(self.generator.xt0[1,0]), str(self.generator.Q), str(self.generator.R), self.seed_value)
+			# plt.plot(process[0], process[1], lw=1.5, markersize = 15, color=color[2], marker=',')
+			# plt.scatter(measure[0], measure[1], s = 50, lw=1.5,color=color[1], marker='+')
+			# plt.plot(output[0], output[1], lw=0.4, markersize = 15, color=color[0], marker='.')
+			plt.plot(process[0], process[1], lw=1.5, markersize=15, marker=',')
+			plt.scatter(measure[0], measure[1], s=50, lw=1.5, marker='+')
+			plt.plot(output[0], output[1], lw=0.4, markersize=15, marker='.')
 			plt.title(title)
 			plt.xlabel(y_label)
 			plt.ylabel(x_label)
+			plt.figtext(.93, .5, "  Parameters \nx0 = ({},{})\nQ={}\nR={}\nts={}".format(str(self.generator.xt0[0,0]), str(self.generator.xt0[1,0]),
+																						 str(self.generator.Q), str(self.generator.R), str(self.ts)))
 			plt.legend(["Process", "Filter", "Measure"])
 			plt.show()
 		elif self.n//2 == 3:
 			title = title + ", seed=" + str(self.seed_value)
 			ax = plt.axes(projection='3d')
-			ax.scatter3D(process[0], process[1], process[2], lw=1.5, color=color[2], marker=',')
-			ax.scatter3D(measure[0], measure[1], measure[2], lw=0.4, color=color[1], marker='+')
-			ax.scatter3D(output[0], output[1], output[2], lw=0.4, color=color[0], marker='.')
+			# ax.scatter3D(process[0], process[1], process[2], lw=1.5, color=color[2], marker=',')
+			# ax.scatter3D(measure[0], measure[1], measure[2], lw=0.4, color=color[1], marker='+')
+			# ax.scatter3D(output[0], output[1], output[2], lw=0.4, color=color[0], marker='.')
+			ax.scatter3D(process[0], process[1], process[2], lw=1.5, marker=',')
+			ax.scatter3D(measure[0], measure[1], measure[2], lw=0.4, marker='+')
+			ax.scatter3D(output[0], output[1], output[2], lw=0.4, marker='.')
 			ax.set_xlabel(x_label)
 			ax.set_ylabel(y_label)
 			ax.set_zlabel(z_label)
+			ax.set_title(title)
 			plt.legend(["Process", "Filter", "Measure"])
 			plt.show()
 		else:
@@ -123,5 +132,5 @@ class Simulation:
 
 
 	
-if __name__ == "__main__":
-	main()
+# if __name__ == "__main__":
+# 	main()
