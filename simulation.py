abf8ed2bd4fc3a97ab79a7b8fcf80aad04b3a6d2
Salvador Balkus <salbalkus@gmail.com>
2021-06-30 22:03:09 -0700

    Improved experiment plotting for running multiple at once

--- simulation.py
+++ simulation.py
@@ -10,19 +10,17 @@ from copy import copy
 
 import random as random
 from mpl_toolkits import mplot3d
-from kalmanfilter2 import KalmanFilter
 from matplotlib.patches import Ellipse
-import math
 
 
 class Simulation:
     def __init__(self, generator, kFilter, seed_value=1):
         """
-	    Constructs a simulation environment for one-line plotting data
+        Constructs a simulation environment for one-line plotting data
 
-		:param generator: Data Generator object
-		:param kFilter: function for predicting the trajectory
-		"""
+        :param generator: Data Generator object
+        :param kFilter: function for predicting the trajectory
+        """
         self.seed_value = seed_value
         self.generator = generator
         self.kFilter = kFilter
@@ -31,22 +29,25 @@ class Simulation:
         self.processes = dict()
         self.measures = dict()
         self.trajectories = dict()
+        self.descs = dict()
         self.ellipses = dict()
-
-    def set_generator(self, generator):
-        self.generator = generator
-
-    def set_filter(self, kFilter):
-        self.kFilter = kFilter
 
     def generate(self, time_steps):
         """
-		Generates process and measurement data
-		"""
+        Generates process and measurement data
+        """
         random.seed(self.seed_value)
         process = self.generator.process(time_steps)
         self.processes[len(self.processes.keys())] = process
         self.measures[len(self.measures.keys())] = self.generator.measure(process)
+        self.descs[len(self.descs.keys())] = {
+            "x0": str(self.generator.xt0[0, 0]),
+            "y0": str(self.generator.xt0[1, 0]),
+            "Tangent Variance": str(self.generator.Q[2, 2]),
+            "Normal Variance": str(self.generator.Q[3, 3]),
+            "Measurement Noise Variance": str(self.generator.R[1, 1]),
+            "Time Steps": str(time_steps)
+        }
 
     def predict(self, index=None, x0=None, Q=None, R=None, H=None, u=None):
         output = np.empty((self.n, 1))
@@ -82,10 +83,21 @@ class Simulation:
             ellipses.append(self.cov_ellipse(mean=mean_, cov=cov_))
         self.trajectories[len(self.trajectories.keys())] = output[:, 1:]  # delete the first column (initial data)
         err_arr = np.array(self.kFilter_model.error_array).squeeze()
-		# self.cov_ellipse(err_arr, np.mean(err_arr, axis = 0), self.kFilter_model.R)
+        # self.cov_ellipse(err_arr, np.mean(err_arr, axis = 0), self.kFilter_model.R)
         self.ellipses[len(self.ellipses.keys())] = ellipses
 
-    def plot(self, index=None, title="Object Position", x_label="x", y_label="y", z_label="z", ax=None, ellipse_freq=0):
+    def experiment(self, ts, **kwargs):
+        for arg in kwargs.items():
+            for value in arg[1]:
+                self.generator = self.generator.mutate(**{arg[0]: value})
+                self.generate(ts)
+                self.predict()
+
+    def experiment_plot(self, ts, var, **kwargs):
+        self.experiment(ts, **kwargs)
+        self.plot_all(var)
+
+    def plot(self, var = "Time Steps", index=None, title="Object Position", x_label="x", y_label="y", z_label="z", ax=None, ellipse_freq=0):
         if index is None:
             index = len(self.processes.keys()) - 1
         process = self.processes[index]
@@ -104,7 +116,9 @@ class Simulation:
             line3, = ax.plot(output[0], output[1], lw=0.4, markersize=8, marker='.')
             lines = [line1, line2, line3]
 
-            ax.set_title(title)
+            # Add the parameters we use. Note that nu is hardcoded as R[0,0] since the measurement noise is independent in both directions
+            #ax.set_title(title + "\n" + self.descs[index], loc="left", y=1)
+            ax.set_title(title + "\n" + var + " = " + str(self.descs[index][var]))
             ax.set_xlabel(y_label)
             ax.set_ylabel(x_label)
             ax.patches = []
@@ -113,8 +127,12 @@ class Simulation:
                     if j % ellipse_freq == 0:
                         new_c=copy(ellipse)
                         ax.add_patch(new_c)
-            # ax.set_aspect(1)
-            # plt.figtext(.93, .5, "  Parameters \nx0 = ({},{})\nQ={}\nR={}\nts={}".format(str(self.generator.xt0[0,0]), str(self.generator.xt0[1,0]),str(self.generator.Q), str(self.generator.R), str(self.measures[index][0].size)))
+            ax.set_aspect(1)
+            ax.axis('square')
+
+            #Below is an old method, if we want to include the full Q and R matrix
+            #plt.figtext(.93, .5, "  Parameters \nx0 = ({},{})\nQ={}\nR={}\nts={}".format(str(self.generator.xt0[0,0]), str(self.generator.xt0[1,0]), str(self.generator.Q), str(self.generator.R), str(self.measures[index][0].size)))
+
             if legend is True:
                 ax.legend(["Process", "Filter", "Measure", "Covariance"])
             return lines
@@ -133,7 +151,7 @@ class Simulation:
         else:
             print("Number of dimensions cannot be graphed.")
 
-    def plot_all(self, ellipse_freq = 0):
+    def plot_all(self, var, ellipse_freq = 0):
         num_plots = len(self.processes)
         num_rows = int(np.ceil(num_plots / 3))
         if num_plots > 3:
@@ -143,7 +161,7 @@ class Simulation:
             plt.subplots_adjust(hspace=.5, bottom=.15)
             lines = []
             for i in range(0, len(self.processes)):
-                lines = self.plot(index=i, title="Plot " + str(i + 1), ax=ax[i // 3, i % 3], ellipse_freq=ellipse_freq)
+                lines = self.plot(index=i, var = var, ax=ax[i // 3, i % 3], ellipse_freq=ellipse_freq)
             if num_plots % 3 == 1:  # one plot on last row
                 ax[num_rows - 1, 1].remove()
             if num_plots % 3 != 0:  # one or two plots
@@ -154,17 +172,18 @@ class Simulation:
                 fig.legend(handles=lines, labels=["Process", "Filter", "Measure"], loc='lower center')
         else:
             self.plot(ellipse_freq=ellipse_freq)
+        plt.tight_layout()
 
     def cov_ellipse(self, mean, cov, zoom_factor=5, p=0.95):
         s = -2 * np.log(1 - p)
         w, v = np.linalg.eig(s*cov)
-        print(v)
         ang = np.arctan2(v[0, 0], v[0, 1]) / np.pi * 180
-        print(ang)
         ellipse = Ellipse(xy=mean, width=zoom_factor*w[0], height=zoom_factor*w[1], angle=ang, edgecolor='g', fc='none', lw=1)
         return ellipse
 
-def cov_ellipse_fancy(X, mean, cov, p=[0.99, 0.95, 0.90]):
+
+
+def cov_ellipse_fancy(X, mean, cov, p=(0.99, 0.95, 0.90)):
     plt.rcParams.update({'font.size': 22})
     fig = plt.figure(figsize=(12, 12))
     # colors = Cube1_4.mpl_colors
@@ -175,7 +194,7 @@ def cov_ellipse_fancy(X, mean, cov, p=[0.99, 0.95, 0.90]):
         s = -2 * np.log(1 - p[i])
         w, v = np.linalg.eig(s * cov)
         # w = np.sqrt(w)
-        ang = np.atan2(v[0, 0], v[1, 0]) / np.pi * 180
+        ang = np.arctan2(v[0, 0], v[1, 0]) / np.pi * 180
         ellipse = Ellipse(xy=mean, width=2 * w[0], height=2 * w[1], angle=ang, lw=2, fc="none", label=str(p[i]))
         cos_angle = np.cos(np.radians(180. - ang))
         sin_angle = np.sin(np.radians(180. - ang))
@@ -190,4 +209,5 @@ def cov_ellipse_fancy(X, mean, cov, p=[0.99, 0.95, 0.90]):
     axes.scatter(X[:, 0], X[:, 1], linewidths=0, alpha=1)
     plt.legend(title="p-value", loc=2, prop={'size': 15}, handleheight=0.01)
     plt.show()
+
 
