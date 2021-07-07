c151030190b53402f824513f1922cb5a984a813c
Nidhi Pai <nidhi.pai@gmail.com>
2021-07-07 11:57:14 -0700

    progress on mtt

--- mtt/tracker2.py
+++ mtt/tracker2.py
@@ -14,7 +14,30 @@ class Tracker:
         for method in self.methods:
             method.predict(tracks=self.tracks)
 
+        print(self.tracks)
+
         self.clear_possible_observations()  # reset this for the next round
+
+    def set_kalman_params(self, f, jac, h, Q, W, R, H, u):
+        self.kalman_params = {
+            'f': f,
+            'jac': jac,
+            'h': h,
+            'Q': Q,
+            'W': W,
+            'R': R,
+            'H': H,
+            'u': u
+        }
+
+
+    # current guess is the entire list of trajectories so far
+    def get_trajectories(self):
+        # get a dictionary of tracks
+        current_guess = dict()
+        for track in self.tracks:
+            current_guess[len(current_guess)] = track.predictions
+        return current_guess
 
     def clear_possible_observations(self):
         for track in self.tracks:
