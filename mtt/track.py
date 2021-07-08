fc388a2eb82de477ff6bea458dabd8d556d33ef2
Nidhi Pai <nidhi.pai@gmail.com>
2021-07-08 09:55:37 -0700

    added track maintence atributes to track class

--- mtt/track.py
+++ mtt/track.py
@@ -5,7 +5,10 @@ class Track:
         self.measurements = []
         self.predictions = []
         self.kfilter = filter_model.__init__()
-        self.possible_observations = [] # just for using to pass from gating to data association
+        self.possible_observations = dict() # just for using to pass from gating to data association
+        self.stage = 0 # 0 is not confirmed yet, 1 is confirmed, 2 is on the way to deletion, 3 is deletion
+        self.initiate_count = 0
+        self.delete_count = 0
 
     def get_current_guess(self):
         return self.kfilter.get_current_guess()
@@ -13,8 +16,12 @@ class Track:
     def get_measurement_cov(self):
         return self.kfilter.R
 
-    def add_measurement(self, time, measurement):
+    def add_measurement(self, measurement):
         self.measurements.append(measurement)
 
-    def add_possible_observations(self, observations):
-        self.possible_observations.append(observations)
+    def add_all_possible_observations(self, observations):
+        for i, obs in enumerate(observations):
+            self.add_possible_observation(i, obs)
+
+    def add_possible_observation(self, index, observation):
+        self.possible_observations[index] = observation
