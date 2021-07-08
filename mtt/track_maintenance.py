d17ef708ccc6757af318f8f3202799c1811fbb29
Eduardo Sosa <ensosa22@colby.edu>
2021-07-08 11:29:18 -0700

    fixed mahalanobis distance

---
+++ mtt/track_maintenance.py
@@ -0,0 +1,12 @@
+"""Aerospace Team - Eduardo Sosa, Nidhi Pai, Sal V Balkus, Tony Zheng"""
+
+import numpy
+
+class TrackMaintenance:
+	def __init__(self, kalman_params, tracks):
+		self.kalman_params
+		self.tracks = tracks
+
+	def new_track(self, measurement):
+
+	def update_tracks(self):
