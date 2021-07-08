a5f87f553409fed058e4de95b0288892c98888dc
Sal Balkus <45985925+salbalkus@users.noreply.github.com>
2021-07-08 14:58:10 -0700

    Update track.py

--- mtt/track.py
+++ mtt/track.py
@@ -2,18 +2,21 @@ import numpy as np
 
 class Track:
     def __init__(self):
-        self.measurements = []
-        self.predictions = []
-        self.possible_observations = dict() # just for using to pass from gating to data association
-        self.stage = 0 # 0 is not confirmed yet, 1 is confirmed, 3 is deletion
+        self.measurements = [] # List of measurements associated with this track
+        self.predictions = [] # One prediction per time step
+        self.possible_observations = dict() # just for using to pass from gating to data association, resets every time step
+        self.stage = 0 # labels stage of the object; 0 is not confirmed yet, 1 is confirmed, 2 is deletion
         #self.initiate_count = 1
-        self.delete_count = 0
+        #self.delete_count = 0
 
     def set_filter(self, filter_model, filter_params):
         # THIS IS DEF NOT RIGHT WAY OF PASSING THE PARAMS AS A DICTIONARY
         self.kfilter = filter_model.__init__(filter_params)
 
     def get_current_guess(self):
+        """"
+        Returns current guess of the Kalman filter
+        """"
         return self.kfilter.get_current_guess()
 
     def get_measurement_cov(self):
