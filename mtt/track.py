3defab4d8567d307948cd71fb5c288954fff89e8
Eduardo Sosa <eduardososa@Eduardos-MacBook-Air.local>
2021-07-09 15:42:40 -0700

    made graphing error across many simulations possible

---
+++ mtt/track.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+class Track:
+    def __init__(self):
+        self.measurements = []
+        self.predictions = []
+        self.possible_observations = dict() # just for using to pass from gating to data association
+        self.stage = 0 # 0 is not confirmed yet, 1 is confirmed, 3 is deletion
+        #self.initiate_count = 1
+        self.delete_count = 0
+
+    def set_filter(self, filter_model, filter_params):
+        # THIS IS DEF NOT RIGHT WAY OF PASSING THE PARAMS AS A DICTIONARY
+        self.kfilter = filter_model.__init__(filter_params)
+
+    def get_current_guess(self):
+        return self.kfilter.get_current_guess()
+
+    def get_measurement_cov(self):
+        return self.kfilter.R
+
+    def add_measurement(self, measurement):
+        self.measurements.append(measurement)
+
+    def add_all_possible_observations(self, observations):
+        for i, obs in enumerate(observations):
+            self.add_possible_observation(i, obs)
+
+    def add_possible_observation(self, index, observation):
+        self.possible_observations[index] = observation
