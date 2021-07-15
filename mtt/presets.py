3416214c623903bcc3a938e6a259a4409755e50f
Salvador Balkus <salbalkus@gmail.com>
2021-07-15 11:29:44 -0700

    restructure pipeline into subfolder and step 2 ellipse plotting

---
+++ mtt/presets.py
@@ -0,0 +1,15 @@
+import mtt
+
+
+class Presets:
+
+	# TODO: Change default parameters to be empirically-tested heuristics
+	@staticmethod
+	def standardSHT(num_objects, params, gate_size=10, gate_expand_size=100, gate_method="euclidean",
+	                num_init=2, num_init_frames=3, num_delete=3):
+		gate = mtt.DistanceGating(gate_size, expand_gating=gate_expand_size, method=gate_method)
+		assoc = mtt.DataAssociation()
+		maintain = mtt.TrackMaintenance(mtt.KalmanFilter, params, num_obj=num_objects,
+		                                num_init=num_init, num_init_frames=num_init_frames, num_delete=num_delete)
+		filter_ = mtt.FilterPredict()
+		return [gate, assoc, maintain, filter_]
