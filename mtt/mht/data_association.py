d4487c796fe8c202ba5217ba8c32e1b7c24c7f35
Tony Zeng <tnzeng@gmail.com>
2021-07-26 09:51:45 -0700

    Retabbing

--- mtt/mht/data_association.py
+++ mtt/mht/data_association.py
@@ -9,18 +9,18 @@ from mtt import Track
 """
 
 class MHTDataAssociation:
-    def __init__(self):
-        pass
+	def __init__(self):
+		pass
 
-    def predict(self, tracks=None, measurements=None, time=None, false_alarms=None, hypotheses=None):
-        for hyp_key, hyp in hypotheses.items():
-            possible_next_tracks = []
-            for track_key, track in hyp.tracks.items():
-                possible_next_tracks.append(track)
-                for i, obs in enumerate(measurements):
-                    for poss_obs in track.possible_observations.values():
-                        if np.array_equiv(obs, poss_obs): # check if obs is in poss_obs
-                            # TODO get kfilter and params from somewhere
-                            new_track = Track(kfilter=kfilter, filter_params=filter_params, init_measure=obs, init_time=time)
-                            tracks[len((tracks))] = new_track  # add track to the master list of tracks
-                            possible_next_tracks.append(new_track)
+	def predict(self, tracks=None, measurements=None, time=None, false_alarms=None, hypotheses=None):
+		for hyp_key, hyp in hypotheses.items():
+			possible_next_tracks = []
+			for track_key, track in hyp.tracks.items():
+				possible_next_tracks.append(track)
+				for i, obs in enumerate(measurements):
+					for poss_obs in track.possible_observations.values():
+						if np.array_equiv(obs, poss_obs): # check if obs is in poss_obs
+							# TODO get kfilter and params from somewhere
+							new_track = Track(kfilter=kfilter, filter_params=filter_params, init_measure=obs, init_time=time)
+							tracks[len((tracks))] = new_track  # add track to the master list of tracks
+							possible_next_tracks.append(new_track)
