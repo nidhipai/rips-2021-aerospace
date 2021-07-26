58bf7d2d2df750acee14b5ffe8f20a0db0808783
Tony Zeng <tnzeng@gmail.com>
2021-07-26 09:34:11 -0700

    Computing best hypothesis by maximum weighted clique.

--- mtt/mht/track.py
+++ mtt/mht/track.py
@@ -5,7 +5,7 @@ class Track:
         self.score = score
         self.x_hat = None
         self.P = None
-        self.observations = starting_observations  # list of (ts, k), where ts is the timestep and k is the number of the measurement
+        self.observations = starting_observations  # list/dict of (ts, k), where ts is the timestep and k is the number of the measurement
         # essentially this is the index in tracker.observations
         self.possible_observations = []  # lists possible observations for this timestep, indexes
         self.status = 0
