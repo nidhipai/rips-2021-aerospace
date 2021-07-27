4e5d953f236e7cf55b803b609f7595b6487ee262
Eduardo Sosa <eduardososa@Eduardos-MacBook-Air.local>
2021-07-27 13:04:27 -0700

    big changes to tracker3, and track maintenance

--- mtt/mht/track.py
+++ mtt/mht/track.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 class Track:
-    def __init__(self, starting_observations, score, object_id, x_hat, P = None):
+    def __init__(self, starting_observations, score, object_id, x_hat, P = None, ts = 0):
         self.score = score
         self.x_hat = x_hat
         self.n = self.x_hat[0].shape[0]
@@ -21,7 +21,7 @@ class Track:
         else:
             self.P = P # posteriori estimate error covariance initialized to the identity matrix
         self.P_minus = self.P
-        self.ts = 0
+        self.ts = ts
         self.missed_measurements = 0
 
     def run_kalman(self, kalman_filter, measurements, ts):
@@ -29,7 +29,7 @@ class Track:
             self.x_hat_minus, self.P_minus = kalman_filter.time_update(self.x_hat, self.P)
             if len(self.possible_observations) != 0:
                 observation = measurements[self.possible_observations[0]]
-                self.observations.append({ts: self.possible_observations[0]})
+                self.observations[ts] = self.possible_observations[0]
                 self.x_hat, self.P = kalman_filter.measurement_update(self.x_hat_minus, self.P_minus, observation)
                 self.possible_observations = []
         self.ts += 1
