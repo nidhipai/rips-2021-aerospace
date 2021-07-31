2d6e995c5a262258322a2db81b91fd1e5f51a856
Tony Zeng <tnzeng@gmail.com>
2021-07-30 17:38:10 -0700

    pruning bug

--- mtt/mht/pruning.py
+++ mtt/mht/pruning.py
@@ -3,41 +3,48 @@
 import numpy as np
 
 class Pruning:
-    def __init__(self, n):
+	def __init__(self, n):
 
-        self.n = n # The number of time steps from which to prune back
+		self.n = n # The number of time steps from which to prune back
 
-    def predict(self, tracks, best_tracks):
-        """
+	def predict(self, tracks, best_tracks):
+		"""
 
-        Extracts and stores the sequences of measurements that correspond to valid tracks.
-        Gathers the required observations that feed into the best tracks
-        before the specified time step and then gets rid of any tracks that do not feed into 
-        those tracks. 
+		Extracts and stores the sequences of measurements that correspond to valid tracks.
+		Gathers the required observations that feed into the best tracks
+		before the specified time step and then gets rid of any tracks that do not feed into 
+		those tracks. 
 
-        Args:
-            tracks (list): a list of all possible tracks at the current time step.
-            best_tracks (list): list of indices of the best tracks selected at the current time step.
-        """
-        
-        required_obs = []
-        for index in best_tracks:
-            prev_obs = np.array(list(tracks[index].observations.values()))
-            required_obs.append(prev_obs[:(prev_obs.size - 1 - self.n)])
-        required_obs = np.array(required_obs)
+		Args:
+			tracks (list): a list of all possible tracks at the current time step.
+			best_tracks (list): list of indices of the best tracks selected at the current time step.
+		"""
 
-        # Test each track to see whether its initial sequence leads to a valid part of the tree
-        for track in tracks:
-            keep = False
-            # Extract the first part of the sequence of measurements, up to n
-            prev_ob = np.array(list(track.observations.values()))
-            prev_ob = prev_ob[:(prev_ob.size - 1 - self.n)]
+		required_obs = []
+		for index in best_tracks:
+			prev_obs = np.array(list(tracks[index].observations.values()))
+			print("P", index, prev_obs)
+			required_obs.append(prev_obs[:(prev_obs.size - 1 - self.n)])
+		required_obs = np.array(required_obs)
+		print("R", required_obs)
 
-            # Test each possibility
-            for required_ob in required_obs:
-                if required_ob.size == prev_ob.size and (required_ob == prev_ob).all():
-                    keep = True
+		# Test each track to see whether its initial sequence leads to a valid part of the tree
+		for track in tracks:
+			keep = False
+			# Extract the first part of the sequence of measurements, up to n
+			prev_ob = np.array(list(track.observations.values()))
+			# TODO: I'm not actually sure if this inequality is quite right.
+			# NOTE: The -1 might/might not be appropriate, and the strictness of the inequality might/might not be appropriate
+			if prev_ob.size - 1 - self.n < 0:
+				continue
+			prev_ob = prev_ob[:(prev_ob.size - 1 - self.n)]
 
-            # Remove the current track if its initial sequence of measurements does not match the current best hypothesis up to n
-            if not keep:
-                tracks.remove(track)
+			# Test each possibility
+			for required_ob in required_obs:
+				if required_ob.size == prev_ob.size and (required_ob == prev_ob).all():
+					keep = True
+
+			# Remove the current track if its initial sequence of measurements does not match the current best hypothesis up to n
+			if not keep:
+				print("THROWN: ", track.obj_id)
+				tracks.remove(track)
